d788fb66d865b42697e97d4a21abf4b942b968be
SergTsch87 <sergtsch87@gmail.com>
2025-05-09 13:50:46 +0300

    Upd: Register Tutor model in Django Admin with decorator

--- tutors_app/admin.py
+++ tutors_app/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Tutor
 
 # Register your models here.
-admin.site.register(Tutor)
+# admin.site.register(Tutor)
+
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject')
+    search_fields = ('name', 'subject')
