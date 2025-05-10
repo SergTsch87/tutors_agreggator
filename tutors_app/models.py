4c367a3bd4e651050b934ed20d4d382bee4d9073
SergTsch87 <sergtsch87@gmail.com>
2025-05-10 17:20:28 +0300

    Upd: Add method get_absolute_url in models.py + upd 1 line in tutor_list.html

--- tutors_app/models.py
+++ tutors_app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Tutor(models.Model):
-    name = models.CharField(max_length=255)
-    subject = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
     def formatted_subject(self):
         return self.subject.title()
+    
+    def get_absolute_url(self):
+        return reverse('tutors:tutor_detail', args=[str(self.pk)])
