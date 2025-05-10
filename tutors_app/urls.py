3e78ff86c754b1e9b3d9640e95fb002f11f623af
SergTsch87 <sergtsch87@gmail.com>
2025-05-10 11:57:24 +0300

    Upd: Update urls.py
    Add new path with TutorDetailView, int:pk + app_name

--- tutors_app/urls.py
+++ tutors_app/urls.py
@@ -2,9 +2,12 @@
 # # Class-Based View (CBV):
 # from tutors_app.views import HomeView
 from django.urls import path
-from .views import TutorListView
+from .views import TutorListView, TutorDetailView
 
+
+app_name = 'tutors'
 
 urlpatterns = [
-    path('', TutorListView.as_view(), name='tutor_list'),
+    path('', TutorListView.as_view(), name='tutors_list'),
+    path('<int:pk>/', TutorDetailView.as_view(), name='tutor_detail'),
 ]
