53ca875dbee93134841a8985e564744d2d374506
SergTsch87 <sergtsch87@gmail.com>
2025-05-11 15:16:41 +0300

    Add: Add method get_context_data in views.py
    Override the get_context_data() method to add the list of all distinct subjects

--- tutors_app/views.py
+++ tutors_app/views.py
@@ -20,6 +20,12 @@ class TutorListView(ListView):
         if subject:
             queryset = queryset.filter(subject__iexact=subject)
         return queryset
+    
+    # Pass the list of subjects to the template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["subjects"] = Tutor.objects.values_list("subject", flat=True).distinct()
+        return context
 
 
 class TutorDetailView(DetailView):
