c565dfe6ea5dfac1f146d0b56c904cbcac8fec71
SergTsch87 <sergtsch87@gmail.com>
2025-05-12 22:44:29 +0300

    Add: Add classs Subject + creates a many-to-one link from Tutor to Subject in models.py

--- tutors_app/models.py
+++ tutors_app/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
 class Tutor(models.Model):
     name = models.CharField(max_length=100)
-    subject = models.CharField(max_length=100)
+    # subject = models.CharField(max_length=100)  # ← This will be changed
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='titors')
 
     def __str__(self):
         return self.name
