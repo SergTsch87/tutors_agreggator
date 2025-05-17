fd02a4a0504d89291f270657f1cd375a924d2be2
SergTsch87 <sergtsch87@gmail.com>
2025-05-17 19:13:28 +0300

    Add: Add var raw_id_fields in class TutorAdmin in admin.py

--- tutors_app/admin.py
+++ tutors_app/admin.py
@@ -2,16 +2,19 @@ from django.contrib import admin
 from .models import Tutor, Subject
 
 # Register your models here.
-# admin.site.register(Tutor)
-
 @admin.register(Tutor)
 class TutorAdmin(admin.ModelAdmin):
     list_display = ('name', 'subject')
     search_fields = ('name', 'subject__name')
     list_filter = ('subject',)
+    raw_id_fields = ('subject',)
 
 
 # admin.site.register(Subject)
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
+
+# admin.site.register(Tutor, TutorAdmin)
+# admin.site.register(Subject)
