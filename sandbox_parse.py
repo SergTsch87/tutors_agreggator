32453e96722717960ff72f0d71e6d9f746533281
SergTsch87 <sergtsch87@gmail.com>
2025-05-23 15:31:43 +0300

    Add: Add func get_element + found objects in .\sandbox_parse.py

--- sandbox_parse.py
+++ sandbox_parse.py
@@ -8,31 +8,43 @@ def get_html(url):
     return response.text
 
 
+def get_element(block_tag, tag_class):
+    return block_tag.select_one(tag_class).get_text(strip=True) if block_tag else "N/A"
+
+
 def parse_tutors_page(html):
     soup = BeautifulSoup(html, "html.parser")
     tutors = []
 
     for block in soup.select(".styles_container__4lrBa"):
-        # Extract the tutos name
-        name_tag = block.select_one(".styles_userName__ltIVo span")
-        name = name_tag.get_text(strip=True) if name_tag else "N/A"
-        #__next > div > section > div.styles_tutorsContainer__y5yzC > div.styles_catalogWrapper__zRZcm > div.styles_mainContent__fp_h3 > div.styles_tutorsList__80R_9 > div:nth-child(1) > div > div.styles_userPreviewWrapper__HBWPS > div.styles_userPreview__dSzlG > p > span
-        #__next > div > section > div.styles_tutorsContainer__y5yzC > div.styles_catalogWrapper__zRZcm > div.styles_mainContent__fp_h3 > div.styles_tutorsList__80R_9
+        # Extract the tutors name
+        # name_tag = block.select_one(".styles_userName__ltIVo span")
+        # name = name_tag.get_text(strip=True) if name_tag else "N/A"
+        name = get_element(block, ".styles_userName__ltIVo span")
+        
+        # Extract the price
         # price_tag = block.select_one(".rate.schoolRate .topCeil")
-        price_tag = block.select_one(".rate .topCeil")
-        price = price_tag.get_text(strip=True) if price_tag else "N/A"
-        tutors.append({"name": name, "price": price})
+        # price_tag = block.select_one(".rate .topCeil")
+        # price = price_tag.get_text(strip=True) if price_tag else "N/A"
+        price = get_element(block, ".rate .topCeil")
+
+        object_item = ''
+        for item in block.find_all('span', class_="styles_lessonsItem__v8FAD"):
+            object_item += item.get_text(strip=True) + ', '
+            print(f'object_item: {object_item}')
+            
+        tutors.append({"name": name, "price": price, "objects": object_item})
 
     return tutors
 
 
 def main():
-    url = "https://buki.com.ua/tutors-online/biolohiia/"
+    url = "https://buki.com.ua/tutors-online/biolohiia/5/"
     html = get_html(url)
     data = parse_tutors_page(html)
 
     for tutor in data:
-        print(f"{tutor['name']} - {tutor['price']}")
+        print(f"Tutor name: {tutor['name']},  Price: {tutor['price']},  Objects: {tutor['objects']}")
 
 
 if __name__ == "__main__":
