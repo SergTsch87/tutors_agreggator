b62b496948ef09b050eebb374cc3689fedbed990
SergTsch87 <sergtsch87@gmail.com>
2025-05-24 08:38:34 +0300

    Add: Add func parse_tutor_card + change func parse_tutors_page  in .\sandbox_parse.py

--- sandbox_parse.py
+++ sandbox_parse.py
@@ -12,38 +12,60 @@ def get_element(block_tag, tag_class):
     return block_tag.select_one(tag_class).get_text(strip=True) if block_tag else "N/A"
 
 
+def parse_tutor_card(html_card):
+    # Extract Data from a Single Tutor Card
+    # Саме в цій функції ми визначаємо усі ті дані, які хочемо дістати з кожної картки репетитора
+    soup = BeautifulSoup(html_card, 'html.parser')
+
+    # Extract the tutors name
+    name = get_element(soup, ".styles_userName__ltIVo span")
+    
+    # Extract the price
+    price = get_element(soup, ".rate .topCeil")
+
+    object_item = ''
+    for item in soup.find_all('span', class_="styles_lessonsItem__v8FAD"):
+        object_item += item.get_text(strip=True) + ', '
+        print(f'object_item: {object_item}')
+
+    ratg_revs = soup.find('div', class_="styles_reviewsBlock__FNrPL") if soup else "N/A"
+    rating = ratg_revs.select_one('span').get_text(strip=True)
+    number_of_reviews = ratg_revs.find('span', class_="styles_reviewsCount__EAIh6").get_text(strip=True)
+
+    main_info = soup.find('div', class_="styles_mainInfo__cK8Ru") if soup else "N/A"
+    # education = main_info.find('p', class_="styles_education__41VXk").select_one('span').get_text(strip=True)
+    education = main_info.find('p', class_="styles_education__41VXk").get_text(strip=True)
+    experience = main_info.find('p', class_="styles_practice__AZyXc").get_text(strip=True)
+    
+    # current_price = extract_element(soup, "div", "ft-whitespace-nowrap ft-text-22 ft-font-bold")
+
+    return {
+            "name": name,
+            "price": price,
+            "objects": object_item,
+            "rating": rating,
+            "number_of_reviews": number_of_reviews,
+            "education": education,
+            "experience": experience
+        }
+
+
 def parse_tutors_page(html):
     soup = BeautifulSoup(html, "html.parser")
+
     tutors = []
-
-    for block in soup.select(".styles_container__4lrBa"):
-        # Extract the tutors name
-        name = get_element(block, ".styles_userName__ltIVo span")
-        
-        # Extract the price
-        price = get_element(block, ".rate .topCeil")
-
-        object_item = ''
-        for item in block.find_all('span', class_="styles_lessonsItem__v8FAD"):
-            object_item += item.get_text(strip=True) + ', '
-            print(f'object_item: {object_item}')
-
-        ratg_revs = block.find('div', class_="styles_reviewsBlock__FNrPL") if block else "N/A"
-        rating = ratg_revs.select_one('span').get_text(strip=True)
-        number_of_reviews = ratg_revs.find('span', class_="styles_reviewsCount__EAIh6").get_text(strip=True)
-
-        main_info = block.find('div', class_="styles_mainInfo__cK8Ru") if block else "N/A"
-        # education = main_info.find('p', class_="styles_education__41VXk").select_one('span').get_text(strip=True)
-        education = main_info.find('p', class_="styles_education__41VXk").get_text(strip=True)
-        experience = main_info.find('p', class_="styles_practice__AZyXc").get_text(strip=True)
-            
-        tutors.append({"name": name, "price": price, "objects": object_item, "rating": rating, "number_of_reviews": number_of_reviews, "education": education, "experience": experience})
+    
+    # for block in soup.select(".styles_container__4lrBa"):
+    tutor_cards = soup.select(".styles_container__4lrBa")
+    for card in tutor_cards:
+        current_card = parse_tutor_card(str(card))
+        tutors.append(current_card)
 
     return tutors
 
 
 def main():
-    url = "https://buki.com.ua/tutors-online/biolohiia/6/"
+    url = "https://buki.com.ua/tutors-online/biolohiia/7/"
     html = get_html(url)
     data = parse_tutors_page(html)
 
