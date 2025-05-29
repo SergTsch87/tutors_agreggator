96a1ac6fbdf28ad512bfc897df53681e7f41c2eb
SergTsch87 <sergtsch87@gmail.com>
2025-05-29 18:07:41 +0300

    Add: Add funcs parse_tutor_card_buki, parse_tutor_card_profrep, parse_tutors_page_buki, parse_tutors_page_profrep, in .\sandbox_parse.py

--- sandbox_parse.py
+++ sandbox_parse.py
@@ -12,20 +12,6 @@ def get_element(block_tag, tag_class):
     return block_tag.select_one(tag_class).get_text(strip=True) if block_tag else "N/A"
 
 
-# def safe_text(soup_or_el, selector=None, class_name=None,  tag='span', default="N/A"):
-#     if not soup_or_el:
-#         return default
-#     try:
-#         if class_name:
-#             el = soup_or_el.find(tag, class_=class_name)
-#         elif selector:
-#             el = soup_or_el.select_one(selector)
-#         else:
-#             return default
-#         return el.get_text(strip=True) if el else default
-#     except Exception:
-#         return default
-
 def safe_text(soup_or_el, selector=None, class_name=None,  tag='span', default="N/A"):
     if not soup_or_el:
         return default
@@ -46,62 +32,93 @@ def safe_text(soup_or_el, selector=None, class_name=None,  tag='span', default="
         return default
 
 
-def parse_tutor_card(html_card):
+def parse_tutor_card_buki(html_card):
     # Extract Data from a Single Tutor Card
     # Саме в цій функції ми визначаємо усі ті дані, які хочемо дістати з кожної картки репетитора
     soup = BeautifulSoup(html_card, 'html.parser')
-
-    # # ratg_revs = soup.find('div', class_="styles_reviewsBlock__FNrPL") if soup else "N/A"
-    # # rating = ratg_revs.select_one('span').get_text(strip=True)
-    # safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span")
-
-    # # number_of_reviews = ratg_revs.find('span', class_="styles_reviewsCount__EAIh6").get_text(strip=True)
-    # safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span", class_name="styles_reviewsCount__EAIh6")
-    
-
-    # # main_info = soup.find('div', class_="styles_mainInfo__cK8Ru") if soup else "N/A"
-    # # # education = main_info.find('p', class_="styles_education__41VXk").select_one('span').get_text(strip=True)
-    # # education = main_info.find('p', class_="styles_education__41VXk").get_text(strip=True)
-    # safe_text(soup.select_one('div.styles_mainInfo__cK8Ru'), "p", class_name="styles_education__41VXk")
-
-    # # experience = main_info.find('p', class_="styles_practice__AZyXc").get_text(strip=True)
-    # safe_text(soup.select_one('div.styles_mainInfo__cK8Ru'), "p", class_name="styles_practice__AZyXc")
-    
-    # current_price = extract_element(soup, "div", "ft-whitespace-nowrap ft-text-22 ft-font-bold")
-
     return {
             "name": get_element(soup, ".styles_userName__ltIVo span"),
             "price": get_element(soup, ".rate .topCeil"),
             "objects": [o.get_text(strip=True) for o in soup.find_all('span', class_="styles_lessonsItem__v8FAD")],
-            "rating": safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span"), # rating,
-            "number_of_reviews": safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span", class_name="styles_reviewsCount__EAIh6"), # number_of_reviews,
-            "education": safe_text(soup.select_one('p.styles_education__41VXk'), "span"), # education,
-            "experience": safe_text(soup.select_one('p.styles_practice__AZyXc')), # experience
+            "rating": safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span"),
+            "number_of_reviews": safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span", class_name="styles_reviewsCount__EAIh6"),
+            "education": safe_text(soup.select_one('p.styles_education__41VXk'), "span"),
+            "experience": safe_text(soup.select_one('p.styles_practice__AZyXc')),
+            "about_myself": None,
+            "city_or_online": None,
         }
 
 
-def parse_tutors_page(html):
+def parse_tutor_card_profrep(html_card):
+    # Extract Data from a Single Tutor Card
+    # Саме в цій функції ми визначаємо усі ті дані, які хочемо дістати з кожної картки репетитора
+    soup = BeautifulSoup(html_card, 'html.parser')
+    return {
+            # "name": get_element(soup, ".card-courses-title h4 span"),
+            # "name": get_element(soup, ".card-courses-title span"),
+            "name": safe_text(soup.select_one('div.card-courses-title'), "span"),
+            
+            "price": None,
+            "objects": None,
+            
+            "rating": None,
+            "number_of_reviews": None,
+            "education": safe_text(soup.select_one('div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(2)')),
+            # col-md-12 catalog-item-desc mt-1
+            # div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(2) > b
+            "experience": safe_text(soup.select_one('div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(1)')),
+            # div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(1) > b
+
+            "about_myself": safe_text(soup.select_one('div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(3)')),
+            # div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(3)
+
+            "city_or_online": safe_text(soup.select_one('i.fa fa-map-marker-alt')),
+        }
+
+
+def parse_tutors_page_buki(html):
     soup = BeautifulSoup(html, "html.parser")
 
     tutors = []
     
-    # for block in soup.select(".styles_container__4lrBa"):
     tutor_cards = soup.select(".styles_container__4lrBa")
     for card in tutor_cards:
-        current_card = parse_tutor_card(str(card))
+        current_card = parse_tutor_card_buki(str(card))
+        tutors.append(current_card)
+
+    return tutors
+
+
+def parse_tutors_page_profrep(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    tutors = []
+    
+    # tutor_cards = soup.select(".row > .col-lg-12.m-b30 > .widget-inner")
+    tutor_cards = soup.select(".widget-inner")
+    # print(f'\ntutor_cards: {tutor_cards}\n')
+    # print(f'\nsize(data): {len(tutor_cards)}\n')
+    # print(f'tutor_cards: {tutor_cards}')
+    for card in tutor_cards:
+        current_card = parse_tutor_card_profrep(str(card))
         tutors.append(current_card)
 
     return tutors
 
 
 def main():
-    url = "https://buki.com.ua/tutors-online/biolohiia/3/"
-    # url = "https://profrepetitor.com.ua/repetitors-biologiya"
+    # url = "https://buki.com.ua/tutors-online/biolohiia/3/"
+    # html = get_html(url)
+    # data = parse_tutors_page_buki(html)
+
+    url = "https://profrepetitor.com.ua/repetitors-biologiya"
     html = get_html(url)
-    data = parse_tutors_page(html)
+    # print(f'\nsize(html): {len(html)}\n')
+    data = parse_tutors_page_profrep(html)
+    # print(f'\nCount elements of data: {len(data)}\n')
 
     for tutor in data:
-        print(f"Tutor name: {tutor['name']},  Price: {tutor['price']},  Objects: {tutor['objects']}, Rating: {tutor['rating']}, Number of reviews: {tutor['number_of_reviews']}, Education: {tutor['education']}, Experience: {tutor['experience']}")
+        print(f"Tutor name: {tutor['name']},  Price: {tutor['price']},  Objects: {tutor['objects']}, Rating: {tutor['rating']}, Number of reviews: {tutor['number_of_reviews']}, Education: {tutor['education']}, Experience: {tutor['experience']}, about_myself: {tutor['about_myself']}, city_or_online: {tutor['city_or_online']}")
 
 
     # html_str = "<p class='styles_education__41VXk'>Освіта: <span>Запорізький державний медичний університет (ЗДМУ)</span></p>"
