14803d0021999339fd044a6838862d933b51db96
SergTsch87 <sergtsch87@gmail.com>
2025-05-30 20:31:37 +0300

    Add, Ran: Add func register_parser in .\decors.py - This test should pass!

--- decors.py
+++ decors.py
@@ -2,3 +2,10 @@ registry = {}
 
 def register(site_name, func):
     registry[site_name] = func
+
+
+def register_parser(site_name):
+    def decorator(func):
+        registry[site_name] = func
+        return func
+    return decorator
