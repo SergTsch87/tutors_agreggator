f1e4d2fdcff0c9980f2a62fc41932f94daba182a
SergTsch87 <sergtsch87@gmail.com>
2025-05-30 22:00:03 +0300

    Add, Ran: Added a condition 'if not isinstance'  in .\decors.py - This test should pass!

--- decors.py
+++ decors.py
@@ -5,6 +5,9 @@ def register(site_name, func):
 
 
 def register_parser(site_name):
+    if not isinstance(site_name, str):
+        raise TypeError(f'Site name must be a string, got {type(site_name).__name__}')
+
     def decorator(func):
         registry[site_name] = func
         return func
