9692cbe88521189ee9e41cd308ec5cfe8d2f92bb
SergTsch87 <sergtsch87@gmail.com>
2025-07-09 18:08:12 +0300

    Add: Added a schema for extracting and saving data for every 20 accounts in sandbox_parse.py

--- sandbox_parse.py
+++ sandbox_parse.py
@@ -3,352 +3,11 @@
 # env1\bin\python -m pip freeze > requirements.txt
 # env2\bin\python -m pip install -r requirements.txt
 
-# import requests
-# from bs4 import BeautifulSoup
-from tutors_app.utils import is_connected, get_file_path, timer_elapsed # parse_price, get_num_of_reviews, handle_exception
-from tutors_app.file_dir_sys import write_list_data_to_file # import create_dir, create_empty_txt_file, create_file_dir_structure, create_and_info_dir, rename_txt_file, get_count_lines_file, writing_html_to_file, write_list_data_to_file
-from tutors_app.scrap_logic import get_tag_body, get_max_pagination, get_tutor_urls
+from tutors_app.utils import is_connected, get_file_path, timer_elapsed
+from tutors_app.file_dir_sys import write_list_data_to_file
+from tutors_app.scrap_logic import get_tag_body, get_max_pagination, get_tutor_urls #, get_element
 from pathlib import Path
-# import zlib
-# from lorem_text import lorem  #  for insert text 'dolorem ipsum')
-# import socket
 import logging
-# import time
-# from functools import lru_cache
-
-# ============ Utility functions ========================
-
-# def is_connected():    # Для перевірки доступності інтернету перед відправленням запиту
-#     try:
-#         socket.create_connection(('www.google.com', 80), timeout=5)
-#         return True
-#     except OSError:
-#         return False
-
-
-# def get_file_path(fname):       # Визначаємо повний шлях до файлу fname
-#     return Path(__file__).parent / fname
-
-
-# # ---------- Decorators --------------------
-# def handle_exception(e, context=""):
-#     """
-#     Handles exceptions and returns a formatted error message.
-
-#     Args:
-#         e (Exception): The exception to handle.
-#         context (str): Additional context about where the error occurred.
-
-#     Returns:
-#         str: Formatted error message
-#     """
-#     error_messages = {
-#         requests.exceptions.Timeout: "Request timed out.",
-#         requests.exceptions.ConnectionError: "Could not connect to the server.",
-#         requests.exceptions.HTTPError: "HTTP error occurred.",
-#         AttributeError: "HTML structure issue.",
-#         ValueError: "Data issue.",
-#         requests.exceptions.RequestException: "Unexpected request error."
-#     }
-
-#     error_message = error_messages.get(type(e), f"Unexpected error: {e}")
-#     logging.error(f"{context} {error_message}")
-#     return error_message
-
-
-# def timer_elapsed(func):   # Для замірювання часу виконання ф-ції func
-#     def wrapper(*args, **kwargs):
-#         start_time = time.time()
-#         result = func(*args, **kwargs)
-#         end_time = time.time()
-#         print(f'Time elapsed in {func.__name__}: {end_time - start_time:.2f} seconds')
-#         return result
-#     return wrapper
-
-# ------------- Parsing logic ---------------------------------
-# def get_html(url: str, timeout=20, return_soup=True):
-#     try:
-#         response = requests.get(url, timeout=timeout, allow_redirects=False)
-#         html = response.text
-#         soup_or_html = BeautifulSoup(html, 'html.parser') if return_soup else html
-#         is_redirect = 1 if 300 <= response.status_code < 400 else 0
-#         return soup_or_html, is_redirect        # 0 == 'No redirect'   # 1 == 'redirect'
-#     except requests.exceptions.RequestException as e:
-#         print(f'An error occured: {e}')
-#         return None, -1  # Уніфікований фолбек на помилку  # always return a tuple
-
-
-# @lru_cache(maxsize = 3000)  # Для кешування повторних URL адрес
-# def fetch_url_with_retries(url, retries=3, timeout=10, return_soup=False):
-#     """
-#     Фактично, ця ф-ція є обгорткою для get_html(), запускаючи останню до 3-х разів, з перервою у 10 сек між повторами
-    
-#     Fetches a URL with a specified number of retries on network-related errors.
-#     #  Fetches the HTML content of a webpage with error handling for network issues.
-
-#     Args:
-#         url (str): The URL of the webpage to fetch.
-#         retries (int): Number of retry attempts.
-#         timeout (int): Timeout in seconds for the request.
-
-#     Returns:
-#         str: The HTML content of the page, or an error message if an exception occurs.
-
-#     Example of use:
-#         html = fetch_url_with_retries(url, retries=3, timeout=10)
-
-#         if not html:
-#             print("Empty HTML or failed to fetch page.")
-#             break
-#     """
-    
-#     if not is_connected():  # Якщо нема інтернет-зв'язку
-#         print('Error: No internet connection')
-#         return None, -1
-#         # return 'Error: No internet connection'
-
-#     # Повтори при таймаутах
-#     for attempt in range(retries):
-#         try:
-#             print(f'Fetching URL: {url}')  # !!! переконайтеся, що ви дійсно отримуєте нову сторінку
-#             html, redirect = get_html(url, timeout=timeout, return_soup=return_soup)
-            
-#             if html:
-#                 return html, redirect  # Успішний запит, - Повертаємо контент
-
-#             # # if html is None or len(html.strip()) == 0:
-#             # #     return []
-#             # if html is None or ( not return_soup and len(html.strip()) == 0 ): # ???
-#             #     return 'Error: Empty HTML content'
-            
-#             # return html, redirect  # Успішний запит, - Повертаємо контент
-        
-#         except requests.RequestException as e:
-#             logging.error(f"Attempt {attempt + 1} failed for {url}.")
-#             if attempt == retries - 1:  # Last attempt
-#                 return handle_exception(e, context=f"Fetching URL {url}")
-#             time.sleep(2 ** attempt)  #  Покрокове збільшення затримки, - задля уникнення блокування сервером
-
-#     return 'Error: Failed to fetch the URL after multiple retries.'  # Якщо усі спроби були невдалі:...
-
-
-# def get_element(block_tag, tag_class):
-#     return block_tag.select_one(tag_class).get_text(strip=True) if block_tag else "N/A"
-
-
-# def safe_text(soup_or_el, selector=None, class_name=None,  tag='span', default="N/A"):
-#     if not soup_or_el:
-#         return default
-    
-#     try:
-#         if selector or class_name:
-#             if class_name:
-#                 el = soup_or_el.find(tag, class_=class_name)
-#             else:
-#                 el = soup_or_el.select_one(selector)
-    
-#         else:
-#             el = soup_or_el  # Вважаємо, що передали вже готовий елемент
-    
-#         return el.get_text(strip=True) if el else default
-    
-#     except Exception:
-#         return default
-
-# # ------------------------------------------------
-# # Site BUKI com
-
-# def parse_tutor_card_buki(html_card):
-#     # Extract Data from a Single Tutor Card
-#     # Саме в цій функції ми визначаємо усі ті дані, які хочемо дістати з кожної картки репетитора
-#     soup = BeautifulSoup(html_card, 'html.parser')
-#     about_myself = soup.select_one('p.styles_description__EnqoA')
-#     return {
-#             "id_tutor": int(soup.select_one(".styles_userName__ltIVo a")["href"][6:-1]),
-#             "name": get_element(soup, ".styles_userName__ltIVo span"),
-#             "price": parse_price(get_element(soup, ".rate .topCeil")),
-#             # "price": get_element(soup, ".rate .topCeil"),
-#             "objects": [o.get_text(strip=True) for o in soup.find_all('span', class_="styles_lessonsItem__v8FAD")],
-#             "rating": safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span"),
-#             # "number_of_reviews": safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span", class_name="styles_reviewsCount__EAIh6"),
-#             "number_of_reviews": get_num_of_reviews(safe_text(soup.select_one('div.styles_reviewsBlock__FNrPL'), "span", class_name="styles_reviewsCount__EAIh6")),
-#             "education": safe_text(soup.select_one('p.styles_education__41VXk'), "span"),
-            
-#             "experience": safe_text(soup.select_one('p.styles_practice__AZyXc'))[15:-6].strip() + '+',
-            
-#             # "about_myself": safe_text(soup.select_one('p.styles_description__EnqoA')),
-#             "about_myself": about_myself.select_one('span').get_text(strip=True) + about_myself.select_one('span.next_sibling').get_text(strip=True),
-            
-#             "city_or_online": safe_text(soup.select_one('div.styles_userData__xpfLk a')),
-#         }
-
-
-# def parse_tutors_page_buki(html):
-#     soup = BeautifulSoup(html, "html.parser")
-
-#     tutors = []
-    
-#     tutor_cards = soup.select(".styles_container__4lrBa")
-#     for card in tutor_cards:
-#         current_card = parse_tutor_card_buki(str(card))
-#         tutors.append(current_card)
-
-#     return tutors
-
-# # ---------------------------------
-# # Site PROFREP
-# def parse_tutor_card_profrep(html_card):
-#     # Extract Data from a Single Tutor Card
-#     # Саме в цій функції ми визначаємо усі ті дані, які хочемо дістати з кожної картки репетитора
-#     soup = BeautifulSoup(html_card, 'html.parser')
-#     return {
-#             "name": safe_text(soup.select_one('div.card-courses-title'), "span"),            
-#             "price": None,
-#             "objects": None,
-#             "rating": None,
-#             "number_of_reviews": None,
-#             "education": safe_text(soup.select_one('div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(2)')),
-#             "experience": safe_text(soup.select_one('div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(1)')),
-#             "about_myself": safe_text(soup.select_one('div.col-md-12.catalog-item-desc.mt-1 > p:nth-child(3)')),
-#             "city_or_online": safe_text(soup.select_one('i.fa fa-map-marker-alt')),
-#         }
-
-
-# def parse_tutors_page_profrep(html):
-#     soup = BeautifulSoup(html, "html.parser")
-
-#     tutors = []
-    
-#     # tutor_cards = soup.select(".row > .col-lg-12.m-b30 > .widget-inner")
-#     tutor_cards = soup.select(".widget-inner")
-#     for card in tutor_cards:
-#         current_card = parse_tutor_card_profrep(str(card))
-#         tutors.append(current_card)
-
-#     return tutors
-
-# ========================================
-# Створення структури папок для пришвидшення розробки веб-скрапера
-
-# def create_dir(dir_path):
-#     '''
-#     dir_path == "my_documents/reports"
-#     Create a directory:
-#         Create a Path object and then use its mkdir() method.
-#         Set "parents=True" to create any missing parent directories,
-#         and "exist_ok=True" to avoid an error if the directory already exists.
-#     '''
-#     dir_path = Path(dir_path)
-#     dir_path.mkdir(parents=True, exist_ok=True)
-#     # print(f"Folder '{dir_path}' created successfully (or already exists).") # for test
-
-
-# def get_list_ids_tutors_on_page(num_page):
-#     pass
-
-# def create_empty_txt_file(file_name):
-#     with open(file_name, 'w'):
-#         pass
-#     # print(f"File '{file_name}' created successfully") # for test
-
-
-# def create_file_dir_structure(subject):
-#     current_directory = Path.cwd()
-#     dir_path = f"{current_directory}/{subject}/"
-#     create_dir(dir_path)
-#     for num_page_dir in range(1, 8):
-#         dir_path_num_page = f"{dir_path}/{str(num_page_dir)}"
-#         create_dir(dir_path_num_page)
-#         create_empty_txt_file(f'{dir_path_num_page}/{num_page_dir}.txt') # html-код за адресою f'/{num_page_dir}/' - для подальшого зберігання списку репетиторів на певній сторінці
-#         for id_tutor_dir in range( 1 + 20 * ( num_page_dir - 1), 1 + 20 * num_page_dir ):
-#             path_id_tutor_dir = f'{dir_path_num_page}/{str(id_tutor_dir)}'
-#             create_dir(f'{path_id_tutor_dir}')
-#             create_empty_txt_file(f'{path_id_tutor_dir}/{id_tutor_dir}.txt') # html-код за адресою f'/{num_page_dir}/{id_tutor_dir}/'
-  
-#         # list_ids_tutors_on_page = get_list_ids_tutors_on_page(num_page_dir)
-#         # for id_tutor_dir in list_ids_tutors_on_page:
-#         #     create_dir(dir_path + '/' + str(num_page_dir) + '/' + str(id_tutor_dir))
-#         #     # create_empty_txt_file() # html-код за адресою f'/{num_page_dir}/{id_tutor_dir}/'
-
-
-# def create_and_info_dir(category_name): # is_exist_dir()
-#     subject_dir = Path('bio')
-#     # return subject_dir.exists()
-
-#     if not subject_dir.exists():
-#         create_file_dir_structure(subject_dir)
-#         print('Такої папки нема, тому Створено задану структуру папок')
-#     else:
-#         print('Така папка вже існує!')
-
-# # --------------------------------------------------
-# def get_count_lines_file(file_path):
-#     with open(file_path, 'r') as file:
-#         line_count = 0
-#         for count, _ in enumerate(file):
-#             line_count = count + 1
-#         # line_count = count + 1 if count is not None else 0 # Handle empty files
-#     return line_count
-
-
-# def writing_html_to_file(num_page, file_name):
-#     url = f"https://buki.com.ua/tutors/biolohiia/{num_page}/"
-#     # html, _ = get_html(url, timeout=10, return_soup=False)
-#     html, _ = fetch_url_with_retries(url, retries=3, timeout=10, return_soup=False)
-
-#     # if html is None:
-#     #     # handle error
-#     if not html or html.startswith('Error'):
-#         print(f"Failed to fetch HTML for page {num_page}")
-#         return
-
-#     file_path = f'{Path.cwd()}/bio/{num_page}/{file_name}/{file_name}.txt'
-
-#     try:
-#         with open(file_path, 'w', encoding='utf-8') as file:
-#             file.write(html)
-#         print(f"Succesfully wrote large text in chunks to {file_path}")
-
-#     except IOError as e:
-#         print(f'Error writing to file: {e}')
-
-
-# def rename_txt_file(old_file_name, new_file_name):
-#     old_file_path = Path(f'{Path.cwd()}/bio/{old_file_name}.txt')
-#     new_file_path = Path(f'{Path.cwd()}/bio/{new_file_name}.txt')
-    
-#     if old_file_path.exists():
-#         old_file_path.rename(new_file_path)
-#     else:
-#         print(f'Не існує файлу {old_file_name}.txt!')
-
-# ========================================
-
-# def write_list_data_to_file(file_path, list_data, mode='a'):
-#     '''
-#         mode == 'a' or 'w'
-#     '''
-#     with open(file_path, mode, encoding='utf-8') as file:
-#         file.write('\n' + '\n'.join(list_data))
-
-
-# def get_tag_body(num_page):
-#     url = f"https://buki.com.ua/tutors/biolohiia/{num_page}/"
-#     soup, _ = fetch_url_with_retries(url, retries=3, timeout=10, return_soup=True)
-#     if soup is None:
-#         return None
-#     # if not soup:
-#     #     return None
-#     tag_body = soup.select_one('body')
-#     return tag_body
-    
-
-# def get_max_pagination(soup_element):
-#     return int( safe_text( soup_element.select(".styles_pagination__qGM14 div a")[-1] ) )
-
-
-# def get_tutor_urls(soup_elem):
-#     return soup_elem.select_one(".styles_userName__ltIVo a")["href"][6:-1]
 
 
 @timer_elapsed
@@ -371,12 +30,20 @@ def main():
 
 # ---------------------
 
+# 09.07.25
+#     Task 1:
+#         Взяти 20 ідентифікаторів → Зберегти їх у файл → Зібрати дані з відповідних URL-адрес → Повторити.
+#         Зберіг - Обробив - Повторив
+
     # ! Розкоментуй, щойно будеш готовий запустити скрипт для збору УСІХ посилань
     # num_page = 1
+
     num_page = 97 # ! for test !
+
     if not is_connected():  # Якщо нема інтернет-зв'язку
         print('Error: No internet connection')
         return 'Error: No internet connection'
+
     tag_body = get_tag_body(num_page)
     max_num_pagination = get_max_pagination(tag_body)
     list_urls_tutors = []
@@ -404,7 +71,19 @@ def main():
             # html = get_html(url_tutor)
             # # Тут буде збереження файлу: код сторінки певного репетитора
 
-        write_list_data_to_file(file_path, list_urls_tutors)
+        write_list_data_to_file(file_path, list_urls_tutors) # Зберіг
+        # Можна й так ф-цію назвати:
+        # save_ids_to_file(ids: list[int], filename: str)
+
+        #  # Обробив:
+            # Використовуючи ці 20 IDs, - витяг дані з сайту за 20-ю URL'ами
+        # fetch_data_from_urls(ids: list[int]) -> list[dict]
+        # process_batches(all_ids: list[int], batch_size: int = 20)
+
+        for id_rep in list_urls_tutors:
+            data_one_account = get_data_one_account(id_rep)
+            save_to_db(data_one_account)
+        
         list_urls_tutors = []
 
         num_page += 1
